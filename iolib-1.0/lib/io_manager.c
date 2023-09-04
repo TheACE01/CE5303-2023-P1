0e057b68dced91d7c020a3ecce93286d1d2281c8
Cvaztian <scyock.03@gmail.com>
2023-09-04 15:25:49 -0600

    Implemented dynamic library

---
+++ iolib-1.0/lib/io_manager.c
@@ -0,0 +1,142 @@
+#include <time.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <config.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#define HIGH                1
+#define LOW                 0
+#define FAIL                -1
+#define SUCCESS             0
+#define READ_MODE           0
+#define WRITE_MODE          1
+#define PIN_VALUE_LEN       30
+#define ADDR_BUFFER_LEN     35
+#define EXPORT_BUFF_LEN     3
+
+void print_error(const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    fprintf(stderr, "Error: ");
+    vfprintf(stderr, message, args);
+    fprintf(stderr, "\n");
+
+    va_end(args);
+}
+
+int get_file_desc(char *path, int mode){
+    int file_desc = open(path, mode);
+    if(file_desc == FAIL)
+        print_error("failed to open file %s in mode %d", path, mode);
+    return file_desc;
+}
+
+int export_pin(int pin) {
+    char buffer[EXPORT_BUFF_LEN];
+    ssize_t written_bytes;
+    int file_desc = open("/sys/class/gpio/export", O_WRONLY);
+    written_bytes = snprintf(buffer, EXPORT_BUFF_LEN, "%d", pin);
+    int op_status = write(file_desc, buffer, written_bytes);
+    close(file_desc);
+    if(op_status == FAIL) {
+        print_error("failed to export pin %d", pin);
+        return FAIL;
+    }
+    return SUCCESS;
+}
+
+int unexport_pin(int pin) {
+    char buffer[EXPORT_BUFF_LEN];
+    ssize_t written_bytes;
+    int file_desc = open("/sys/class/gpio/unexport", O_WRONLY);
+    written_bytes = snprintf(buffer, EXPORT_BUFF_LEN, "%d", pin);
+    int op_status = write(file_desc, buffer, written_bytes);
+    close(file_desc);
+    if(op_status == FAIL) {
+        print_error("failed to unexport pin %d", pin);
+        return FAIL;
+    }
+    return SUCCESS;
+}
+
+int set_pin_mode(int pin, int mode) {
+    char gpio_path[ADDR_BUFFER_LEN];
+    snprintf(gpio_path, ADDR_BUFFER_LEN, "/sys/class/gpio/gpio%d/direction", pin);
+    int file_desc = get_file_desc(gpio_path, O_WRONLY);
+    
+    int op_status = SUCCESS;
+    if(mode == READ_MODE)
+        op_status = write(file_desc, "in", 2);
+    else if(mode == WRITE_MODE)
+        op_status = write(file_desc, "out", 3);
+    else
+        print_error("unsupported pin mode %d", mode);
+    close(file_desc);
+
+    if(op_status == FAIL) {
+        print_error("failed to set mode %d to pin: %d", mode, pin);
+        return FAIL;
+    }
+    return SUCCESS;
+}
+
+int write_digital(int pin, int value) {
+    char pin_path[ADDR_BUFFER_LEN];
+    snprintf(pin_path, ADDR_BUFFER_LEN, "/sys/class/gpio/gpio%d/value", pin);
+    int file_desc = get_file_desc(pin_path, O_WRONLY); 
+
+    int op_status = SUCCESS;
+    if(value == HIGH)
+        op_status = write(file_desc, "0", 1);
+    else if(value == LOW)
+        op_status = write(file_desc, "1", 1);
+    close(file_desc);
+
+    if(op_status == FAIL) {
+        print_error("failed to write to file %s", pin_path);
+        return FAIL;
+    }
+    return SUCCESS;
+}
+
+int read_digital(int pin) {
+    char pin_value_path[PIN_VALUE_LEN];
+    snprintf(pin_value_path, PIN_VALUE_LEN, "/sys/class/gpio/gpio%d/value", pin);
+    int file_desc = get_file_desc(pin_value_path, O_RDONLY);
+    
+    char value_str[3];
+    int op_status = read(file_desc, value_str, 3);
+    close(file_desc);
+
+    if(op_status == FAIL) {
+        print_error("failed to read file %s", pin_value_path);
+        return FAIL;
+    }
+    return atoi(value_str);
+}
+
+int blink(int pin, float freq, double duration) {
+    if(freq > 0) {
+        float period = 1/freq;
+
+        clock_t start_time = clock();
+        float elapsed_time = 0;
+        while(elapsed_time < duration){
+            write_digital(pin, HIGH);
+            sleep(period);
+            write_digital(pin, LOW);
+            sleep(period);
+            elapsed_time += 2*period;
+        }
+    }
+    else{
+        print_error("frequency value must be greater than 0");
+        return FAIL;
+    }
+    return SUCCESS;
+}
--- iolib-1.0/src/test.c
+++ iolib-1.0/src/test.c
@@ -1,6 +1,29 @@
 #include <io_manager.h>
-int main(int argc, char const *argv[])
-{
-say_hello();
-return 0;
+#include <stdio.h>
+int main(int argc, char const* argv[] ){
+    // Inicializar pin 4 para escribir en el
+    export_pin(4);
+    set_pin_mode(4, 1);
+    write_digital(4, 1);
+    // blink en el pin 4 por 5 s
+    blink(4, 1, 5);
+
+    // Inicializar pin 17 para leer su valor
+    export_pin(17);
+    set_pin_mode(17, 0);
+    int valor = 0;
+    int indice = 0;
+    // lectura del pin 10 veces para prueba
+    while (indice <= 10)
+    {
+        valor = read_digital(17);
+        printf("Leyendo %d de %d\n", valor, 17);
+        indice++;
+        sleep(1);
+    }
+
+    // retirar los pines
+    unexport_pin(4);
+    unexport_pin(17);
+    return 0;
 }
