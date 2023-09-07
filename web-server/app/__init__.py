f659aa67e1868a0e50adc815d50216297f734502
Kevin Acevedo Rodriguez <keviinar51@gmail.com>
2023-09-06 18:02:04 -0600

    Finished Flask server

--- web-server/app/__init__.py
+++ web-server/app/__init__.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-cors = CORS(app)
+
+# Apply cors policies to manage who can use the 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})  # Replace with your specific origin
 
 from app import routes
